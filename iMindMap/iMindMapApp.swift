44117deceb63cb70d21c6aa2384432b1bc61f002
Michelle <aryanto.michelle@gmail.com>
2024-05-22 22:47:34 +0700

    Initial Commit

---
+++ iMindMap/iMindMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMindMapApp.swift
+//  iMindMap
+//
+//  Created by Michelle Angela Aryanto on 22/05/24.
+//
+
+import SwiftUI
+
+@main
+struct iMindMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
